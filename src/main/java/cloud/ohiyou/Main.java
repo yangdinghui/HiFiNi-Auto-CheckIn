2cd95b7d1676bc208d0eb6e41157ab56f60e0988
yangdh <yangdinghui@cykjc.com>
2024-06-17 13:51:19 +0800

    增加飞书提醒

--- src/main/java/cloud/ohiyou/Main.java
+++ src/main/java/cloud/ohiyou/Main.java
@@ -6,6 +6,7 @@ package cloud.ohiyou;
  */
 
 import cloud.ohiyou.utils.DingTalkUtils;
+import cloud.ohiyou.utils.FeiShuUtils;
 import cloud.ohiyou.utils.TelegramUtils;
 import cloud.ohiyou.utils.WeChatWorkUtils;
 import cloud.ohiyou.vo.CookieSignResult;
@@ -139,6 +140,8 @@ public class Main {
         WeChatWorkUtils.pushBotMessage(WXWORK_WEBHOOK, title, messageBuilder.toString(), "markdown"); // 推送企业微信机器人
         DingTalkUtils.pushBotMessage(DINGTALK_WEBHOOK, title, messageBuilder.toString(), "", "markdown"); // 推送钉钉机器人
         TelegramUtils.publishTelegramBot(TG_CHAT_ID, TG_BOT_TOKEN, "HiFiNi签到消息: \n" + title + "：\n" + messageBuilder.toString()); // push telegram bot
+        FeiShuUtils.sendNotice("HiFiNi签到消息: \n" + title + "：\n" + messageBuilder); // 推送飞书
+
     }
 
 
---
+++ src/main/java/cloud/ohiyou/utils/FeiShuUtils.java
@@ -0,0 +1,60 @@
+package cloud.ohiyou.utils;
+
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yangdh
+ * @date 2024/6/17 11:30
+ */
+public class FeiShuUtils {
+    private static final Logger log = Logger.getLogger(FeiShuUtils.class);
+
+    private static final String FEI_SHU_WEBHOOK = System.getenv("FEI_SHU_WEBHOOK");
+
+    private static final OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build();
+
+    public static void sendNotice(String msg) {
+        sendNotice(msg, FEI_SHU_WEBHOOK);
+    }
+
+    public static void sendNotice(String msg, String webhook) {
+        if (webhook == null || webhook.isEmpty()) {
+            System.out.println("FEI_SHU_WEBHOOK 飞书环境变量未设置");
+            return;
+        }
+        String json = String.format("{ \"msg_type\":\"text\", \"content\":{\"text\":\"%s\"} }", msg);
+        try {
+            RequestBody body = RequestBody.create(json, MediaType.parse("application/json; charset=utf-8"));
+            Request request = new Request.Builder()
+                    .url(webhook)
+                    .post(body)
+                    .build();
+            Call call = client.newCall(request);
+            try (Response response = call.execute()) {
+                assert response.body() != null;
+                log.info("飞书消息发送成功：" + response.body().string());
+            }
+        } catch (Exception e) {
+            log.error("飞书消息发送失败", e);
+        } finally {
+            client.dispatcher().executorService().shutdown();
+            client.connectionPool().evictAll();
+        }
+    }
+
+    public static void main(String[] args) {
+        sendNotice("Hello, World!");
+    }
+}
